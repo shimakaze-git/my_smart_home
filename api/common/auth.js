06b471222f7c226a0c881ade41c166a16aa898a8
shimakaze-git <shimakaze.soft+github@googlemail.com>
2021-09-23 18:45:43 +0900

    fix: fsやメソッドをいくつか復活させる.

--- api/common/auth.js
+++ api/common/auth.js
@@ -1,6 +1,6 @@
-// const fs = require('fs')
+const fs = require('fs')
 const { GoogleSpreadsheet } = require('google-spreadsheet')
-// const keyFile = process.env.keyFile || 'my_smart_home.json'
+const keyFile = process.env.keyFile || 'my_smart_home.json'
 
 class SpreadSheetService {
 
@@ -24,68 +24,68 @@ class SpreadSheetService {
     })
   }
 
-  // /**
-  //  * 行データを返す
-  //  * @param {*} index
-  //  */
-  // async getRows(index) {
-  //   await this.doc.loadInfo()
-  //   const sheet = this.doc.sheetsByIndex[index]
-  //   return sheet.getRows()
-  // }
+  /**
+   * 行データを返す
+   * @param {*} index
+   */
+  async getRows(index) {
+    await this.doc.loadInfo()
+    const sheet = this.doc.sheetsByIndex[index]
+    return sheet.getRows()
+  }
 
-  // /**
-  //  * 行を追加する
-  //  * @param {*} value
-  //  */
-  // async insert(value) {
-  //   await this.doc.loadInfo()
-  //   const sheet = this.doc.sheetsByIndex[0]
-  //   return await sheet.addRow(value);
-  // }
+  /**
+   * 行を追加する
+   * @param {*} value
+   */
+  async insert(value) {
+    await this.doc.loadInfo()
+    const sheet = this.doc.sheetsByIndex[0]
+    return await sheet.addRow(value);
+  }
 
-  // /**
-  //  * データを取得する
-  //  * @param {*} callBack
-  //  */
-  // async select(callBack) {
-  //   const rows = await this.getRows(0)
-  //   const data = []
-  //   for (const row of rows) {
-  //     if (callBack(row)) {
-  //       data.push({id: row.id, name: row.name, age:row.age})
-  //     }
-  //   }
-  //   return data
-  // }
+  /**
+   * データを取得する
+   * @param {*} callBack
+   */
+  async select(callBack) {
+    const rows = await this.getRows(0)
+    const data = []
+    for (const row of rows) {
+      if (callBack(row)) {
+        data.push({id: row.id, name: row.name, age:row.age})
+      }
+    }
+    return data
+  }
 
-  // /**
-  //  * idに紐づくユーザーの情報を更新する
-  // */
-  // async updateById(id, value) {
-  //   const rows = await this.getRows(0);
-  //   for (const row of rows) {
-  //     if (row.id == id) {
-  //       for (const attr in value) {
-  //         row[attr] = value[attr]
-  //         await row.save()
-  //       }
-  //     }
-  //   }
-  // }
+  /**
+   * idに紐づくユーザーの情報を更新する
+  */
+  async updateById(id, value) {
+    const rows = await this.getRows(0);
+    for (const row of rows) {
+      if (row.id == id) {
+        for (const attr in value) {
+          row[attr] = value[attr]
+          await row.save()
+        }
+      }
+    }
+  }
 
-  // /**
-  //  * idに紐づくユーザーを削除する
-  //  * @param {*} id
-  //  */
-  // async deleteById(id) {
-  //   const rows = await this.getRows(0);
-  //   for (const row of rows) {
-  //     if (row.id == id) {
-  //       await row.delete()
-  //     }
-  //   }
-  // }
+  /**
+   * idに紐づくユーザーを削除する
+   * @param {*} id
+   */
+  async deleteById(id) {
+    const rows = await this.getRows(0);
+    for (const row of rows) {
+      if (row.id == id) {
+        await row.delete()
+      }
+    }
+  }
 }
 
 const authentication = async (spread_sheet_key) => {
@@ -93,18 +93,14 @@ const authentication = async (spread_sheet_key) => {
   const service = await new SpreadSheetService(spread_sheet_key)
 
   let credit = null
-  // if (fs.existsSync(keyFile)) {
-  //   // 認証情報jsonファイルを読み込む
-  //   credit = require('../' + keyFile)
-  // } else {
-  //   credit = {
-  //     client_email: process.env.clientEmail,
-  //     private_key: process.env.privateKey,
-  //   }
-  // }
-  credit = {
-    client_email: process.env.clientEmail,
-    private_key: process.env.privateKey,
+  if (fs.existsSync(keyFile)) {
+    // 認証情報jsonファイルを読み込む
+    credit = require('../' + keyFile)
+  } else {
+    credit = {
+      client_email: process.env.clientEmail,
+      private_key: process.env.privateKey,
+    }
   }
   await service.authorize(credit)
   return service
