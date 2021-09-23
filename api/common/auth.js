c32d71363ce68fad33df8ba0147a73246e3dfd47
shimakaze-git <shimakaze.soft+github@googlemail.com>
2021-09-23 17:31:32 +0900

    refactor: await service.authorizeを復活させる.

--- api/common/auth.js
+++ api/common/auth.js
@@ -26,68 +26,68 @@ class SpreadSheetService {
     })
   }
 
-  /**
-   * 行データを返す
-   * @param {*} index
-   */
-  async getRows(index) {
-    await this.doc.loadInfo()
-    const sheet = this.doc.sheetsByIndex[index]
-    return sheet.getRows()
-  }
+  // /**
+  //  * 行データを返す
+  //  * @param {*} index
+  //  */
+  // async getRows(index) {
+  //   await this.doc.loadInfo()
+  //   const sheet = this.doc.sheetsByIndex[index]
+  //   return sheet.getRows()
+  // }
 
-  /**
-   * 行を追加する
-   * @param {*} value
-   */
-  async insert(value) {
-    await this.doc.loadInfo()
-    const sheet = this.doc.sheetsByIndex[0]
-    return await sheet.addRow(value);
-  }
+  // /**
+  //  * 行を追加する
+  //  * @param {*} value
+  //  */
+  // async insert(value) {
+  //   await this.doc.loadInfo()
+  //   const sheet = this.doc.sheetsByIndex[0]
+  //   return await sheet.addRow(value);
+  // }
 
-  /**
-   * データを取得する
-   * @param {*} callBack
-   */
-  async select(callBack) {
-    const rows = await this.getRows(0)
-    const data = []
-    for (const row of rows) {
-      if (callBack(row)) {
-        data.push({id: row.id, name: row.name, age:row.age})
-      }
-    }
-    return data
-  }
+  // /**
+  //  * データを取得する
+  //  * @param {*} callBack
+  //  */
+  // async select(callBack) {
+  //   const rows = await this.getRows(0)
+  //   const data = []
+  //   for (const row of rows) {
+  //     if (callBack(row)) {
+  //       data.push({id: row.id, name: row.name, age:row.age})
+  //     }
+  //   }
+  //   return data
+  // }
 
-  /**
-   * idに紐づくユーザーの情報を更新する
-  */
-  async updateById(id, value) {
-    const rows = await this.getRows(0);
-    for (const row of rows) {
-      if (row.id == id) {
-        for (const attr in value) {
-          row[attr] = value[attr]
-          await row.save()
-        }
-      }
-    }
-  }
+  // /**
+  //  * idに紐づくユーザーの情報を更新する
+  // */
+  // async updateById(id, value) {
+  //   const rows = await this.getRows(0);
+  //   for (const row of rows) {
+  //     if (row.id == id) {
+  //       for (const attr in value) {
+  //         row[attr] = value[attr]
+  //         await row.save()
+  //       }
+  //     }
+  //   }
+  // }
 
-  /**
-   * idに紐づくユーザーを削除する
-   * @param {*} id
-   */
-  async deleteById(id) {
-    const rows = await this.getRows(0);
-    for (const row of rows) {
-      if (row.id == id) {
-        await row.delete()
-      }
-    }
-  }
+  // /**
+  //  * idに紐づくユーザーを削除する
+  //  * @param {*} id
+  //  */
+  // async deleteById(id) {
+  //   const rows = await this.getRows(0);
+  //   for (const row of rows) {
+  //     if (row.id == id) {
+  //       await row.delete()
+  //     }
+  //   }
+  // }
 }
 
 const authentication = async (spread_sheet_key) => {
@@ -104,7 +104,7 @@ const authentication = async (spread_sheet_key) => {
       private_key: process.env.privateKey,
     }
   }
-  // await service.authorize(credit)
+  await service.authorize(credit)
   return service
 }
 
